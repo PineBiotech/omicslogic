fbc2f3c0c1162ff26330616fa9c75863b348c8b3
PineBio <elia@pine-biotech.com>
2020-08-04 17:34:14 -0500

    Update answers_part2.R

--- answers_part2.R
+++ answers_part2.R
@@ -3,9 +3,15 @@
 library(openxlsx)
 
 #load data
-ReasonJoin <- read.xlsx('Answers to questions1.xlsx', sheet = 6)
-Answers <- read.xlsx('Answers to questions1.xlsx', sheet = 7)
+ReasonJoin <- read.xlsx("Answers to questions1.xlsx", sheet = 6)
+Answers1 <- read.xlsx("Answers to questions1.xlsx", sheet = 7)
 
+#write my first function
+ReasonJoin <- data.frame(lapply(ReasonJoin, function(x) {
+  gsub("From ", "", x)
+}))
+
+#create a table of results
 table1 <- table(ReasonJoin$Score1)
 
 sum(table1)
@@ -19,10 +25,11 @@ barplot(table1, col=rainbow(length(Answers$Answer)), main="Summary Bar Plot", na
 par(mar=c(2,2,2,2))
 pie(table1, col=rainbow(length(Answers$Answer)), main="Summary Pie Chart", c(Answers$Answer))
 
-library(tidyverse)
+#View Group of students by Type
+library(sjPlot)     #to make nice tables: https://strengejacke.github.io/sjPlot/articles/table_css.html
+
+tab_df(All4)
 
 All4 <- ReasonJoin[ReasonJoin$Score1 == "6",]
-View(All4)
 
-CleanList <- All4$Name %>% str_replace_all("From", "")
-View(CleanList)
+tab_df(ReasonJoin, title = "Program Interest", sort.column = 4, alternate.rows = TRUE, CSS = list(css.centeralign = 'text-align: left;', css.firsttablerow = 'font-weight: bold;'))
